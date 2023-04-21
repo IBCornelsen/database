dd42c97447baa92855cdd38907cd6eb536f84f1c
Carl-UB-VM <carl.mahnke@gmail.com>
2023-04-21 11:14:12 +0200

    adding dokumententypen and dokumententemplates

--- scripts/09-dokumententemplates.sql
+++ scripts/09-dokumententemplates.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS public.documenttemplates (
 	CONSTRAINT "PK_DOCUMENTTEMPLATES" PRIMARY KEY (id)
 ) ;
 
-INSERT INTO documenttemplates (name, shortdescription, longdescription, user_id, is_private, documenttype, filename, created_at, updated_at) VALUES
-('expose', 'Gebäude Exposé', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,', 1, 'f', 1, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11'),
-('energieausweis', 'Energieausweis', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquy', 1, 'f', 2, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11'),
-('gutachten', 'Gutachten', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, s', 1, 't', 3, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11');
+-- INSERT INTO documenttemplates (name, shortdescription, longdescription, user_id, is_private, documenttype, filename, created_at, updated_at) VALUES
+-- ('expose', 'Gebäude Exposé', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,', 1, 'f', 1, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11'),
+-- ('energieausweis', 'Energieausweis', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquy', 1, 'f', 2, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11'),
+-- ('gutachten', 'Gutachten', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, s', 1, 't', 3, '123e4567-e89b-12d3-a456-426614174000', '2023-04-13 09:57:11','2023-04-13 09:57:11');
 
 ALTER TABLE documenttemplates
   ADD CONSTRAINT USERS_FK FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE;
