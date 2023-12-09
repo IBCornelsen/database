d0873aaf42a005656df5a2d65ee03aa0917e4498
Moritz Utcke <moritz.utcke@gmx.de>
2023-12-09 09:55:12 +0530

    Add TypeGraphQL

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { resolvers } from "./generated/type-graphql";
+import { buildSchema } from "type-graphql";
 
 
 export * from ".prisma/client/index.d";
 export * from "./types/fake-data"
 export const prisma = new PrismaClient()
+export const schema = await buildSchema({
+  resolvers,
+  validate: false,
+});
--- src/models/Postleitzahlen.ts
+++
@@ -1,8 +0,0 @@
-import { Field, ObjectType, Int } from 'type-graphql'
-
-@ObjectType()
-export class TYPEPostleitzahlenGQL {
-  @Field((_type) => Int)
-  id: number
-
-  // skip overwrite 👇
