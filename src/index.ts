6c29c86b7243efa19140911b121d568bc24a773b
Moritz Utcke <moritz.utcke@gmx.de>
2024-01-02 19:01:02 +0700

    Postleitzahlen und Klimafaktoren

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { resolvers } from "./generated/type-graphql";
-import { buildSchema } from "type-graphql";
 
-
-export * from ".prisma/client/index.d";
+export * from "@prisma/client";
 export * from "./types/fake-data"
 export const prisma = new PrismaClient()
-export const schema = await buildSchema({
-  resolvers,
-  validate: false,
-});
